16c3078ba9142dd76061fe4e876b1bf74414f3ae
Hamza Amor <hamza.amor93@gmail.com>
2019-02-13 15:28:23 +0100

    commit ex 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,15 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create({ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurant1 = Restaurant.create(name: 'Dishoom', phone_number: '01 94 53 87 23', address: 'St Martins Lane', category: 'japanese')
+
+restaurant2 = Restaurant.create(name: 'Pitaya', phone_number: '01 74 36 74 75', address: 'Rue Oberkampf', category: 'chinese')
+
+restaurant3 = Restaurant.create(name: 'Eataly', phone_number: '01 63 63 94 73', address: 'Leicester street', category: 'italian')
+
+restaurant4 = Restaurant.create(name: 'Leon', phone_number: '01 03 57 80 20', address: 'Avenue de Waterloo', category: 'belgian')
+
+restaurant5 = Restaurant.create(name: 'Relais', phone_number: '01 94 74 73 39', address: 'St Germain', category: 'French')
